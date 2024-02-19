9e3ba32033f525d7369c12fbaee800632d16f9f2
username <email@email.com>
2024-02-19 16:41:08 +0300

    java-rest-api-test

--- src/test/java/com/example/apitest/app/RestApiControllerWebFluxTest.java
+++ src/test/java/com/example/apitest/app/RestApiControllerWebFluxTest.java
@@ -2,15 +2,13 @@ package com.example.apitest.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
-class RestApiControllerTest {
+@WebFluxTest(controllers = RestApiController.class)
+class RestApiControllerWebFluxTest {
     @Test
     void getResponse(@Autowired WebTestClient client) {
         client.post()
